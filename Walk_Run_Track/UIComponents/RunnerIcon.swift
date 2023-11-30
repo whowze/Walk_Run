eff71d4cea5c456982b71f0d9fe865939b3b0700
Bill Howze <williamlhowze@gmail.com>
2023-11-29 16:36:46 -0800

    Updated the map and runner (#2)
    * Updating map content
    
    * More updates:
    
    - shortened the runner code

--- Walk_Run_Track/UIComponents/RunnerIcon.swift
+++ Walk_Run_Track/UIComponents/RunnerIcon.swift
@@ -8,20 +8,24 @@
 import SwiftUI
 
 struct RunnerIcon: View {
+    let color: Color
     
     var body: some View {
         Image(systemName: "figure.run.circle")
             .resizable()
             .scaledToFit()
             .frame(width: 100, height: 100)
-            .foregroundStyle(Color(.systemBlue))
+            .foregroundStyle(Color(color))
             .shadow(radius: 5)
     }
 }
 
-
-#Preview {
-    RunnerIcon()
+struct RunnerIcon_Preview: PreviewProvider {
+    static var previews: some View {
+        VStack(spacing: 50) {
+            RunnerIcon(color: .blue)
+            RunnerIcon(color: .yellow)
+            RunnerIcon(color: .red)
+        }
+    }
 }
-
-
