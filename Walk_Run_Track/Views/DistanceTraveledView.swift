9183882436c5a608c188c38d62aea0def198de33
Bill Howze <williamlhowze@gmail.com>
2023-11-29 16:43:14 -0800

    small corrections

--- Walk_Run_Track/Views/DistanceTraveledView.swift
+++ Walk_Run_Track/Views/DistanceTraveledView.swift
@@ -1,5 +1,5 @@
 //
-//  DistanceTravledView.swift
+//  DistanceTraveledView.swift
 //  Walk_Run_Track
 //
 //  Created by William Howze on 11/29/23.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct DistanceTravledView: View {
+struct DistanceTraveledView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    DistanceTravledView()
+    DistanceTraveledView()
 }
