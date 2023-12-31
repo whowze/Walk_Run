4aa3ebcf29e8f4345d49f4f01180d54e108a6723
Bill Howze <williamlhowze@gmail.com>
2023-12-31 15:15:04 -0800

    Add Large Map View and Navigation (#3)
    Add navigation from RunnersDetailsView Map -> FullScreenMapView

--- Walk_Run_Track/RunnersDetailsView.swift
+++ Walk_Run_Track/RunnersDetailsView.swift
@@ -11,40 +11,45 @@ struct RunnersDetailsView: View {
     let customButtonFrame = CGSize(width: 350, height: 100)
     
     var body: some View {
-        VStack {
-            MapView()
-                .frame(width: customButtonFrame.width, height: customButtonFrame.height)
+        NavigationView {
+            VStack {
+                NavigationLink(destination: FullScreenMapView()) {
+                    MapView()
+                        .frame(width: customButtonFrame.width, height: customButtonFrame.height)
+                }
+                
+                .clipShape(RoundedRectangle(cornerRadius: 25))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(Color.black, lineWidth: 2)
+                )
+                .padding()
+                
+                VStack {
+                    Text("Timer Running")
+                        .frame(width: customButtonFrame.width, height: customButtonFrame.height)
+                }
+                .overlay(
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(Color.black, lineWidth: 2)
+                )
+                .padding()
+                
+                VStack {
+                    Text("Distance Traveled")
+                        .frame(width: customButtonFrame.width, height: customButtonFrame.height)
+                }
+                .overlay(
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(Color.black, lineWidth: 2)
+                )
+                .padding()
+                
+                // Map button
+                
+                // Pause and Stop
+            }
         }
-        .clipShape(RoundedRectangle(cornerRadius: 25))
-        .overlay(
-            RoundedRectangle(cornerRadius: 25)
-                .stroke(Color.black, lineWidth: 2)
-        )
-        .padding()
-            
-        VStack {
-            Text("Timer Running")
-                .frame(width: customButtonFrame.width, height: customButtonFrame.height)
-        }
-        .overlay(
-            RoundedRectangle(cornerRadius: 25)
-                .stroke(Color.black, lineWidth: 2)
-        )
-        .padding()
-            
-        VStack {
-            Text("Distance Traveled")
-                .frame(width: customButtonFrame.width, height: customButtonFrame.height)
-        }
-        .overlay(
-            RoundedRectangle(cornerRadius: 25)
-                .stroke(Color.black, lineWidth: 2)
-        )
-        .padding()
-            
-        // Map button
-            
-        // Pause and Stop
     }
 }
         
